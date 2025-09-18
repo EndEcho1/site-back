f81cb2aa831baf22baef3d0f4e38c01f7a0ef98c
Echo RE <reecho66ffcc@gmail.com>
2025-09-18 12:44:05 +0800

    Changes

---
+++ echo-flux/src/test/java/com/example/echoflux/EchoFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echoflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
